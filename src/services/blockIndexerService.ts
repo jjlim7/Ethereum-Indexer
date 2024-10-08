424be9e8257ff95adb97ab423302f0097824165a
jjlim7 <jj.limmm159@gmail.com>
2024-10-09 01:53:00 +0800

    Update blockIndexer to decode uniswap event data

--- src/services/blockIndexerService.ts
+++ src/services/blockIndexerService.ts
@@ -1,7 +1,16 @@
 import Web3 from "web3";
-import { Transaction } from "../models/transactionModel";
 import config from "../config/config";
+import { Transaction } from "../models/transactionModel";
 import { getEthPriceAtTime } from "./ethPriceService";
+
+interface IUniswapAbi {
+  sender: string;
+  amount0: bigint;
+  amount1: bigint;
+  sqrtPriceX96: bigint;
+  liquidity: bigint;
+  tick: bigint;
+}
 
 export class BlockIndexer {
   private web3: Web3;
@@ -27,43 +36,101 @@ export class BlockIndexer {
     });
 
     subscription.on("data", async (data) => {
-      this.processBlock(data.blockNumber!, data.transactionHash!);
+      this.processBlock(data.transactionHash!);
     });
   }
 
-  private async processBlock(blockNumber: any, txHash: string) {
+  private async processBlock(txHash: string) {
     try {
       const tx = await this.web3.eth.getTransaction(txHash);
       const receipt = await this.web3.eth.getTransactionReceipt(txHash);
       const block = await this.web3.eth.getBlock(tx.blockNumber);
+
       const ethToUsdtPrice = await getEthPriceAtTime();
-
-      if (!tx.blockNumber) {
-        return;
-      }
-
       const transactionFeeInETH =
         (Number(receipt.gasUsed) * Number(tx.gasPrice)) / 1e18; // Convert Wei to ETH
       const transactionFeeInUSDT = transactionFeeInETH * ethToUsdtPrice;
 
-      const transaction = new Transaction({
-        hash: tx.hash,
-        blockNumber: tx.blockNumber,
-        from: tx.from,
-        to: tx.to,
-        gasUsed: receipt.gasUsed,
-        gasPrice: tx.gasPrice,
-        timeStamp: new Date(Number(block.timestamp) * 1000),
-        transactionFeeInUSDT: transactionFeeInUSDT,
-      });
+      const contractAddress = config.targetAddress;
 
-      console.log(transaction);
+      for (const txlog of receipt.logs) {
+        if (txlog.address!.toLowerCase() != contractAddress.toLowerCase()) {
+          continue;
+        }
 
-      await transaction.save();
+        // Get uniswap event object
+        const swapData = this.decodeTxLog(txlog);
 
-      console.log(`Saved transaction ${tx.hash}`);
+        const transaction = new Transaction({
+          hash: txlog.transactionHash,
+          blockNumber: txlog.blockNumber,
+          from: swapData.sender,
+          to: contractAddress,
+          gasUsed: receipt.gasUsed,
+          gasPrice: tx.gasPrice,
+          value: swapData.amount0,
+          timeStamp: new Date(Number(block.timestamp) * 1000),
+          transactionFeeInUSDT: transactionFeeInUSDT,
+        });
+
+        console.log(transaction);
+
+        await transaction.save();
+
+        console.log(`Saved transaction ${tx.hash}`);
+      }
     } catch (error) {
-      console.error(`Error processing blockNumber ${blockNumber}:`, error);
+      console.error(`Error processing txHash ${txHash}:`, error);
     }
+  }
+
+  private decodeTxLog(txlog: any) {
+    // Decode ABI-encoded log data and indexed topic data to get Uniswap event value
+    const topics = txlog.topics!;
+    const inputs = [
+      {
+        type: "address",
+        name: "sender",
+        indexed: true,
+      },
+      {
+        type: "address",
+        name: "recipient",
+        indexed: true,
+      },
+      {
+        type: "int256",
+        name: "amount0",
+      },
+      {
+        type: "int256",
+        name: "amount1",
+      },
+      {
+        type: "uint160",
+        name: "sqrtPriceX96",
+      },
+      {
+        type: "uint128",
+        name: "liquidity",
+      },
+      {
+        type: "int24",
+        name: "tick",
+      },
+    ];
+
+    const decodedAbi = this.web3.eth.abi.decodeLog(inputs, txlog.data!, topics);
+
+    const swapData: IUniswapAbi = {
+      sender: decodedAbi["sender"] as string, // Address
+      amount0: decodedAbi["amount0"] as bigint, // Convert int256 to string
+      amount1: decodedAbi["amount1"] as bigint, // Convert int256 to string
+      sqrtPriceX96: decodedAbi["sqrtPriceX96"] as bigint, // Convert uint160 to string
+      liquidity: decodedAbi["liquidity"] as bigint, // Convert uint128 to string
+      tick: decodedAbi["tick"] as bigint,
+    };
+
+    return swapData;
   }
 }

b1ef2497438a3f0ebc09e1460bfaa5487d8106ad
jjlim7 <jj.limmm159@gmail.com>
2024-10-09 22:18:48 +0800

    Add swagger ui

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import express, { Application } from 'express';
-import dotenv from 'dotenv';
-import transactionRoutes from './routes/transactionRoutes';
+import express, { Application } from "express";
+import dotenv from "dotenv";
+import transactionRoutes from "./routes/transactionRoutes";
+import swaggerUi from "swagger-ui-express";
+import swaggerOutput from "./swagger_output.json";
 
 dotenv.config();
 
@@ -10,6 +12,8 @@ const app: Application = express();
 app.use(express.json());
 
 // Routes
-app.use('/api', transactionRoutes);
+app.use("/api", transactionRoutes);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerOutput));
 
 export default app;
---
+++ src/swagger.ts
@@ -0,0 +1,20 @@
+import swaggerAutogen from "swagger-autogen";
+
+const doc = {
+  info: {
+    version: "v1.0.0",
+    title: "Swagger Demo Project",
+    description: "Implementation of Swagger with TypeScript",
+  },
+  servers: [
+    {
+      url: "http://localhost:3000",
+      description: "",
+    },
+  ],
+};
+
+const outputFile = "./swagger_output.json";
+const endpointsFiles = ["./src/routes/transactionRoutes.ts"];
+
+swaggerAutogen({ openapi: "3.0.0" })(outputFile, endpointsFiles, doc);
